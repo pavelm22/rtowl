b04a28396501631f9b546b07a8e20a06f0dffca4
PavelC132 <malcharek.pawel@my.dbb-lippe.de>
2025-03-02 14:44:13 +0100

    team refactored

--- assets/views/TeamView.vue
+++ assets/views/TeamView.vue
@@ -4,7 +4,7 @@ import Team from "@/components/Pages/Team/Team.vue";
 </script>
 
 <template>
-<Team team-structure="" handle-menu-click="" active-page=""
+<Team></Team>
 </template>
 
 <style scoped>
