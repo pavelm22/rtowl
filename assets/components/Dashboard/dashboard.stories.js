9659cd6ae6039aea03b80b46d2052b7e92245636
pavel <pavel.malcharek@tecalor.de>
2025-03-30 13:44:57 +0000

    storybook

---
+++ assets/components/Dashboard/dashboard.stories.js
@@ -0,0 +1,10 @@
+import Dashboard from "@/components/Dashboard/Dashboard.vue";
+
+export default {
+    name: "Dashboard",
+    component: Dashboard,
+};
+
+export const Default = {
+    args: {},
+};
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -18,72 +18,72 @@ class ApiController extends AbstractController
     }
 
     // Team Routes
-    #[Route('/teams', name: 'get_teams', methods: ['GET'])]
+    #[Route('/api/teams', name: 'get_teams', methods: ['GET'])]
     public function getTeams(): JsonResponse
     {
         return $this->apiService->getAllTeams();
     }
 
-    #[Route('/teams/{id}', name: 'get_team', methods: ['GET'])]
+    #[Route('/api/teams/{id}', name: 'get_team', methods: ['GET'])]
     public function getTeam(int $id): JsonResponse
     {
         return $this->apiService->getTeamById($id);
     }
 
-    #[Route('/teams/{id}/details', name: 'get_team_details', methods: ['GET'])]
+    #[Route('/api/teams/{id}/details', name: 'get_team_details', methods: ['GET'])]
     public function getTeamDetails(int $id): JsonResponse
     {
         return $this->apiService->getTeamDetailsById($id);
     }
 
-    #[Route('/teams', name: 'create_team', methods: ['POST'])]
+    #[Route('/api/teams', name: 'create_team', methods: ['POST'])]
     public function createTeam(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         return $this->apiService->createTeam($data);
     }
 
-    #[Route('/teams/{id}', name: 'update_team', methods: ['PUT'])]
+    #[Route('/api/teams/{id}', name: 'update_team', methods: ['PUT'])]
     public function updateTeam(Request $request, int $id): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         return $this->apiService->updateTeam($id, $data);
     }
 
-    #[Route('/teams/{id}', name: 'delete_team', methods: ['DELETE'])]
+    #[Route('/api/teams/{id}', name: 'delete_team', methods: ['DELETE'])]
     public function deleteTeam(int $id): JsonResponse
     {
         return $this->apiService->deleteTeam($id);
     }
 
     // Sponsor Routes
-    #[Route('/sponsors', name: 'get_sponsors', methods: ['GET'])]
+    #[Route('/api/sponsors', name: 'get_sponsors', methods: ['GET'])]
     public function getSponsors(): JsonResponse
     {
         return $this->apiService->getAllSponsors();
     }
 
-    #[Route('/sponsors/{id}', name: 'get_sponsor', methods: ['GET'])]
+    #[Route('/api/sponsors/{id}', name: 'get_sponsor', methods: ['GET'])]
     public function getSponsor(int $id): JsonResponse
     {
         return $this->apiService->getSponsorById($id);
     }
 
-    #[Route('/sponsors', name: 'create_sponsor', methods: ['POST'])]
+    #[Route('/api/sponsors', name: 'create_sponsor', methods: ['POST'])]
     public function createSponsor(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         return $this->apiService->createSponsor($data);
     }
 
-    #[Route('/sponsors/{id}', name: 'update_sponsor', methods: ['PUT'])]
+    #[Route('/api/sponsors/{id}', name: 'update_sponsor', methods: ['PUT'])]
     public function updateSponsor(Request $request, int $id): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         return $this->apiService->updateSponsor($id, $data);
     }
 
-    #[Route('/sponsors/{id}', name: 'delete_sponsor', methods: ['DELETE'])]
+    #[Route('/api/sponsors/{id}', name: 'delete_sponsor', methods: ['DELETE'])]
     public function deleteSponsor(int $id): JsonResponse
     {
         return $this->apiService->deleteSponsor($id);
